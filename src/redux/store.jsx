3a76d5b1b9156b46dcb8c8a862bb26763f5efb9d
polly <polifibliss@gmail.com>
2025-03-30 21:26:12 +0300

    fix

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import contactsReducer from './contactsSlice';
 import filtersReducer from './filtersSlice';
 
-const store = configureStore({
+export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
     filters: filtersReducer,
