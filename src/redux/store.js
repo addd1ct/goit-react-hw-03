f6f87429ccb05d89d0de080f1c6d28e59affdec6
polly <polifibliss@gmail.com>
2025-04-10 20:49:14 +0300

    del persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,21 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
 import contactsReducer from './contactsSlice';
 import filtersReducer from './filtersSlice';
 
-const contactsPersistConfig = {
-  key: 'contacts',
-  storage,
-};
-
-const persistedContactsReducer = persistReducer(contactsPersistConfig, contactsReducer);
-
 export const store = configureStore({
   reducer: {
-    contacts: persistedContactsReducer,
+    contacts: contactsReducer,
     filters: filtersReducer,
   },
 });
-
-export const persistor = persistStore(store);
