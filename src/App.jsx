771673d6b48238e82a48b1d08d0a6ece7e16bc0e
polly <polifibliss@gmail.com>
2025-04-27 20:34:13 +0300

    edit structure

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchContacts } from './redux/contacts/contactsOps';
 import { selectLoading, selectError } from './redux/contacts/contactsSlice';
-import ContactForm from './components/contactForm';
-import ContactList from './components/contactList';
-import SearchBox from './components/searchBox';
 import Layout from './components/layout';
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
@@ -20,13 +17,11 @@ const App = () => {
   return (
     <BrowserRouter>
       <Layout>
-        <h1>Phonebook</h1>
-        <SearchBox />
-        {loading && <p>Loading...</p>}
-        {error && <p>Error: {error}</p>}
         <Routes>
-          <Route path="/contacts" element={<ContactList />} />
-          <Route path="/" element={<ContactForm />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegistrationPage />} />
+          <Route path="/contacts" element={<ContactsPage />} />
         </Routes>
       </Layout>
     </BrowserRouter>
---
+++ src/pages/homePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Phonebook App</h1>
+      <p>Here you can manage your contacts.</p>
+    </div>
+  );
+};
+
+export default HomePage;
